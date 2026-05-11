fa1887869c0f05f684ca4a76c3eb08477d95f3b5
ThakurVijay210901 <105584645+ThakurVijay2191@users.noreply.github.com>
2026-05-11 11:56:09 +0530

    Project Setup

---
+++ Vynk/VynkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VynkApp.swift
+//  Vynk
+//
+//  Created by Vijay Thakur on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct VynkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VynkTests/VynkTests.swift
@@ -0,0 +1,19 @@
+//
+//  VynkTests.swift
+//  VynkTests
+//
+//  Created by Vijay Thakur on 11/05/26.
+//
+
+import Testing
+@testable import Vynk
+
+struct VynkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
