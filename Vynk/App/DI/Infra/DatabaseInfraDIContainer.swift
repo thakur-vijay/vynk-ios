defc9bea706a3c83895ad351d4b3a98eaba0605e
ThakurVijay210901 <105584645+ThakurVijay2191@users.noreply.github.com>
2026-06-26 19:20:56 +0530

    Feature first modifications

---
+++ Vynk/App/DI/Infra/DatabaseInfraDIContainer.swift
@@ -0,0 +1,25 @@
+//
+//  DatabaseInfraDIContainer.swift
+//  Vynk
+//
+//  Created by Vijay Thakur on 26/06/26.
+//
+
+import VynkDatabaseKit
+
+final class DatabaseInfraDIContainer {
+    
+    lazy var appDatabase: AppDatabase = {
+        do {
+            return try AppDatabase(migrations: migrations)
+        } catch {
+            fatalError("Failed to initialize database: \(error)")
+        }
+    }()
+    
+    private let migrations: [DatabaseMigration] = [
+        CreateChatListsMigration(),
+        CreateDeviceContactsMigration(),
+        SeedDefaultChatListsMigration()
+    ]
+}
